9f4ca380f3350d53606c93755ad7918d599dfd1d
Tyler Mitchell <tyler106@ksu.edu>
2020-09-09 16:50:39 -0500

    add speedy c++ ac reweighter

---
+++ scripts/fast_ac_reweighting.py
@@ -0,0 +1,45 @@
+from glob import glob
+from pprint import pprint
+from subprocess import call
+
+
+def to_reweight(ifile):
+    """List of signal samples. Only processes these files."""
+    for name in [
+        'vbf125_JHU.root', 'wh125_JHU.root', 'zh125_JHU.root'
+    ]:
+        if name in ifile:
+            return True
+    return False
+
+
+def main(args):
+    input_directories = [idir for idir in glob('{}/*'.format(args.input)) if not 'logs' in idir]
+    input_files = {
+        idir: [ifile for ifile in glob('{}/merged/*.root'.format(idir)) if to_reweight(ifile)]
+        for idir in input_directories
+    }
+    print 'Directory structure to process'
+    pprint(input_files, width=150)
+
+    temp_name = args.input
+    if '/hdfs' in temp_name:
+        temp_name = 'tmp/{}'.format(args.input.split('/')[-1])
+        call('mkdir -p {}'.format(temp_name), shell=True)
+
+    i = 0
+    ndir = len(input_files.keys())
+    for idir, files in input_files.iteritems():
+        i += 1
+        print '\n\033[92m Begin directory {} of {} \033[0m'.format(i, ndir)
+        for ifile in files:
+            call('bin/ac-reweight -n {} -t {} -o {}'.format(ifile, args.tree_name, temp_name), shell=True)
+            call('mv -v {} {}'.format(ifile, ifile + "/../"), shell=True) # move from "merged" to parent directory
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--input', '-i', required=True, help='path to input files')
+    parser.add_argument('--tree-name', '-t', required=True, help='name of TTree')
+    main(parser.parse_args())
