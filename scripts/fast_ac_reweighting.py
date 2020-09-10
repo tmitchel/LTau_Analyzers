7e4ade84a8c672878bf62fd8acefce5fed7ea03d
Tyler Mitchell <tyler106@ksu.edu>
2020-09-10 09:21:13 -0500

    more small fixes

--- scripts/fast_ac_reweighting.py
+++ scripts/fast_ac_reweighting.py
@@ -35,11 +35,11 @@ def main(args):
         for ifile in tqdm(files, leave=False):
             if '/hdfs' in args.input:
                 call('bin/ac-reweight -n {} -t {} -o {}/'.format(ifile, args.tree_name, temp_name), shell=True)
-                call('mv {}/*.root {}'.format(temp_name, ifile.split('/')[-1]), shell=True)
+                call('mv {}/*.root {}'.format(temp_name, idir), shell=True)
             else:
                 call('bin/ac-reweight -n {} -t {} -o {}/merged'.format(ifile, args.tree_name, idir), shell=True)
             fname = ifile.split('/')[-1]
-            call('mv -v {} {}'.format(ifile, ifile.replace(fname, '/merged')), shell=True) # move from "merged" to parent directory
+            call('mv {} {}'.format(ifile, ifile.replace(fname, '/merged')), shell=True) # move from "merged" to parent directory
 
 
 if __name__ == "__main__":
